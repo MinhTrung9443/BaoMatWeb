2ff5764e7361ce399f26eb0f70b3f88ceed9c115
MinhTrung9443 <minhtrungbttv@gmail.com>
2025-05-17 09:41:22 +0700

    them cau hinh chuyen thanh https

--- src/main/java/vn/iotstar/config/SecurityConfig.java
+++ src/main/java/vn/iotstar/config/SecurityConfig.java
@@ -1,18 +1,14 @@
 package vn.iotstar.config;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 
 import vn.iotstar.service.impl.CustomerUserDetailsService;
 
@@ -20,18 +16,23 @@ import vn.iotstar.service.impl.CustomerUserDetailsService;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+    
     @Autowired
     private CustomerUserDetailsService userDetailsService;
+    
     @Autowired
-	private AuthenticationProvider authenticationProvider;
+    private AuthenticationProvider authenticationProvider;
+    
     @Autowired
-	private JwtAuthenticationFilter jwtAuthenticationFilter;
-
-
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    	return http
+        return http
                 .csrf(csrf -> csrf.disable())
+                // Thêm cấu hình HTTPS
+                .requiresChannel(channel -> channel
+                        .anyRequest().requiresSecure())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/User/**").hasAnyAuthority("USER", "VENDOR")
                         .requestMatchers("/Vendor/**").hasAnyAuthority("VENDOR")
@@ -43,6 +44,4 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
-
-
 }
